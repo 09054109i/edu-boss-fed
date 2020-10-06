cc037fb2918754a89a2829e6524fa1d8189b2ef4
linhaiyan <940666845@qq.com>
2020-10-06 15:17:41 +0800

    基于类的Vue组件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
   <div id="app">
     <h1>拉钩教育</h1>
+    <div>{{a}}</div>
     <router-view/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({
-  name: 'App',
-  data () {
-    return {
-      a: 1,
-      b: '2'
-    }
-  },
-  methods: {
-    test () {
-      return this.a.test()
-    }
+import Component from 'vue-class-component'
+
+@Component
+export default class App extends Vue {
+  a = 1
+  b = '2'
+  test () {
+    console.log('test')
   }
-})
+}
 </script>
 <style lang="scss" scoped>
 </style>
