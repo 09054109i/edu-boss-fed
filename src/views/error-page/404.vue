adacd7543e203fb00bf2f1421c3636544738c439
linhaiyan <940666845@qq.com>
2020-10-06 23:19:28 +0800

    初始化路由组件

---
+++ src/views/error-page/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>404 Not Found.</h1>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'CourseIndex'
+})
+</script>
+<style scoped lang = "sass">
+</style>
