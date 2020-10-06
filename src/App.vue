5be6f9385ef147b240efd469eb68caa0d568bc9d
linhaiyan <940666845@qq.com>
2020-10-06 22:36:01 +0800

    接口处理-封装请求模块

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,14 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import request from '@/utils/request'
+
+request({
+  method: 'GET',
+  url: '/boss/v2/api-docs?group=edu-boss-boot'
+}).then(res => {
+  console.log(res)
+})
 
 export default Vue.extend({
   name: 'App'
