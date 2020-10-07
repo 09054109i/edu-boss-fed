00f4a8efdc2b95fb2c2a773410bcfd594b593a76
linhaiyan <940666845@qq.com>
2020-10-07 17:51:50 +0800

    登录-页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,58 @@
 <template>
  <div class="login">
-     用户登录
+  <el-form label-position="top" ref="form" :model="form" label-width="80px">
+    <el-form-item label="手机号">
+      <el-input v-model="form.name"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="form.name"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
+    </el-form-item>
+  </el-form>
  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 })
 </script>
-<style scoped lang = "sass">
+<style scoped lang = "scss">
+  .login {
+     min-height: 100vh;
+     display: flex;
+     align-items: center;
+     justify-content: center;
+    .login-form {
+      width: 300px;
+      background: #fff;
+      padding: 20px;
+      border-radius: 5px;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
 </style>
