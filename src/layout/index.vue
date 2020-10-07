9a7f81a95763b4233c40bfb2ed50b51be36edff2
linhaiyan <940666845@qq.com>
2020-10-07 16:52:31 +0800

    侧边栏菜单

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,16 +1,24 @@
 <template>
   <el-container>
-    <el-aside width="200px">Aside</el-aside>
+    <el-aside width="200px">
+        <Aside/>
+    </el-aside>
     <el-container>
       <el-header>Header</el-header>
-      <el-main>Main</el-main>
+      <el-main>
+          <router-view/>
+      </el-main>
     </el-container>
   </el-container>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import Aside from './components/app-aside.vue'
 export default Vue.extend({
-  name: 'LayoutIndex'
+  name: 'LayoutIndex',
+  components: {
+    Aside
+  }
 })
 </script>
 <style scoped lang = "scss">
