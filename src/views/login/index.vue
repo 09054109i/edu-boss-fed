2e57756770434c72658121798e2cbeea7de39cd5
linhaiyan <940666845@qq.com>
2020-10-07 22:50:46 +0800

    前端-表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,15 @@
 <template>
  <div class="login">
-  <el-form label-position="top" ref="form" :model="form" label-width="80px">
-    <el-form-item label="手机号">
+  <el-form label-position="top"
+            ref="form"
+            :model="form"
+            :rules="rules"
+            label-width="80px">
+    <el-form-item label="手机号" prop="phone">
       <el-input v-model="form.phone"></el-input>
     </el-form-item>
-    <el-form-item label="密码">
-      <el-input v-model="form.password"></el-input>
+    <el-form-item label="密码" prop="password">
+      <el-input type="password" v-model="form.password"></el-input>
     </el-form-item>
     <el-form-item>
       <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
@@ -17,6 +21,7 @@
 import Vue from 'vue'
 import request from '@/utils/request.ts'
 import qs from 'qs'
+import { Form } from 'element-ui'
 
 export default Vue.extend({
   name: 'LoginIndex',
@@ -25,31 +30,45 @@ export default Vue.extend({
       form: {
         phone: '18201288771',
         password: '111111'
+      },
+      rules: {
+        phone: [
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { pattern: /^1\d{10}$/, message: '请输入11个数字', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 11, message: '长度在 6 到 11 个字符', trigger: 'blur' }
+        ]
       }
     }
   },
   methods: {
     async onSubmit () {
-      console.log('submit!')
-      // 1、表单验证
-      // 2、验证通过，提交表单
-      const { data } = await request({
-        method: 'POST',
-        url: '/front/user/login',
-        headers: { 'content-type': 'application/x-www-form-urlencoded' },
-        data: qs.stringify(this.form)
-      })
-      console.log(data)
-      // 3、处理请求结果
-      // 失败：给出提示
-      if (data.state !== 1) {
-        return this.$message.error(data.message)
+      try {
+        // 1、表单验证，
+        await (this.$refs.from as Form).validate()
+        // 2、验证通过，提交表单
+        const { data } = await request({
+          method: 'POST',
+          url: '/front/user/login',
+          headers: { 'content-type': 'application/x-www-form-urlencoded' },
+          data: qs.stringify(this.form)
+        })
+        console.log(data)
+        // 3、处理请求结果
+        // 失败：给出提示
+        if (data.state !== 1) {
+          return this.$message.error(data.message)
+        }
+        // 成功：跳转到首页
+        this.$router.push({
+          name: 'home'
+        })
+        this.$message.success('登录成功')
+      } catch (err) {
+        console.log('登录失败', err)
       }
-      // 成功：跳转到首页
-      this.$router.push({
-        name: 'home'
-      })
-      this.$message.success('登录成功')
     }
   }
 })
