b858ba0648e21b281912e5eb7be7b41fe0fde456
linhaiyan <940666845@qq.com>
2020-10-08 18:16:00 +0800

    axios错误处理

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -28,6 +28,16 @@ request.interceptors.response.use(function (response) { // 状态码为2XX都进
 }, function (error) { // 超出2XX状态码都执行这里
   // 服务端使用过的HTTP状态码，错误处理就写到这里
   console.log('响应失败')
+  if (error.response) { // 请求发出去收到响应了，但是状态码超出了2XX
+    console.log(error.response.data)
+    console.log(error.response.status)
+    console.log(error.response.headers)
+  } else if (error.request) { // 请求发出去但是没有收到响应
+    console.log(error.request)
+  } else { // 在设置请求时发生了一些事情，出发了一个错误
+    console.log('Error', error.message)
+  }
+  console.log(error.config)
   // 将错误信息抛给调用者
   return Promise.reject(error)
 })
