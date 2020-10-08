f3ca89bc7f5c0f38a0e96e601c46563bad2d132e
linhaiyan <940666845@qq.com>
2020-10-08 15:04:16 +0800

    展示当前登录用户信息

--- src/layout/components/app-header.vue
+++ src/layout/components/app-header.vue
@@ -8,20 +8,36 @@
      </el-breadcrumb>
     <el-dropdown>
         <span class="el-dropdown-link">
-            <el-avatar shape="square" :size="40" src="https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png"></el-avatar>
+            <el-avatar shape="square" :size="40" :src="userInfo.portrait || require('@/assets/default-image.jpg')"></el-avatar>
             <i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>用户ID</el-dropdown-item>
-            <el-dropdown-item divided>推出</el-dropdown-item>
+            <el-dropdown-item>{{userInfo.userName}}</el-dropdown-item>
+            <el-dropdown-item divided>退出</el-dropdown-item>
         </el-dropdown-menu>
     </el-dropdown>
  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import { getUserInfo } from '@/services/user'
+
 export default Vue.extend({
-  name: 'AppHeader'
+  name: 'AppHeader',
+  data () {
+    return {
+      userInfo: null
+    }
+  },
+  created () {
+    this.loaderUserInfo()
+  },
+  methods: {
+    async loaderUserInfo () {
+      const { data } = await getUserInfo()
+      this.userInfo = data.content
+    }
+  }
 })
 </script>
 <style scoped lang = "scss">
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,5 +1,6 @@
 import request from '@/utils/request'
 import qs from 'qs'
+import store from '@/store'
 
 /**
  * 用户请求
@@ -13,4 +14,12 @@ export const login = (data: User) => request({
   url: '/front/user/login',
   // headers: { 'content-type': 'application/x-www-form-urlencoded' },
   data: qs.stringify(data)
+})
+export const getUserInfo = () => request({
+  method: 'GET',
+  url: '/front/user/getInfo',
+  // headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  headers: {
+    Authorization: store.state.user.access_token
+  }
 })
