ae777560285c082af9b22c5440f425febf41cad1
linhaiyan <940666845@qq.com>
2020-10-09 21:31:44 +0800

    展示菜单列表

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -4,18 +4,67 @@
       <div slot="header" class="clearfix">
         <el-button @click="$router.push({name: 'menu-create'})">添加菜单</el-button>
       </div>
-      <div v-for="o in 4" :key="o" class="text item">
-        {{'列表内容 ' + o }}
-      </div>
+      <el-table
+        :data="menu"
+        style="width: 100%">
+        <el-table-column
+          label="编号"
+          type="index"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="菜单名称"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="level"
+          label="菜单级数">
+        </el-table-column>
+        <el-table-column
+          prop="icon"
+          label="前端图标">
+        </el-table-column>
+        <el-table-column
+          prop="orderNum"
+          label="排序">
+        </el-table-column>
+         <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import { getAllMenus } from '@/services/menu'
 
 export default Vue.extend({
   name: 'MenuIndex',
+  data () {
+    return {
+      menu: []
+    }
+  },
+  created () {
+    this.loadAllMenus()
+  },
   methods: {
+    async loadAllMenus () {
+      const { data } = await getAllMenus()
+      if (data.code === '000000') {
+        this.menu = data.data
+      }
+    }
   }
 })
 </script>
