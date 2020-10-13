fd2c31c56b904706e3c937700d6d0bd3112b81a7
wb-lhy658549@autonavi.com <lhy09054109>
2020-10-13 20:10:46 +0800

    1

--- src/views/role/allocResource.vue
+++ src/views/role/allocResource.vue
@@ -39,54 +39,54 @@ export default Vue.extend({
         label: 'name'
       }
     }
-  },
-  created () {
-    this.loadmMenuNodeList()
-    this.loadCheckedMenu()
-  },
-  methods: {
-    async handleUpdate () {
-      const checkedKeys = (this.$refs.menuNodes as any).getCheckedKeys()
-      const { data } = await allocateMenus({
-        menuIdList: checkedKeys,
-        roleId: (this.$route as any).params.id
-      })
-      console.log(data)
-    },
-    handleClear () {
-      this.$nextTick(() => {
-        (this.$refs.menuNodes as any).setCheckedKeys([])
-      })
-    },
-    async loadmResourceNodeList () {
-      const { data } = await getMenuNodeList()
-      if (data.code === '000000') {
-        this.menuNodes = data.data
-        // this.loadCheckedMenu()
-      }
-    },
-    async loadCheckedMenu () {
-      const roleId = (this.$route as any).params.id
-      const { data } = await getRoleMenu(roleId)
-      const roleMenuList = data.data
-      roleMenuList.forEach((item: any) => {
-        if (item.selected) {
-          this.defaultCheckedKeys.push(item.id)
-        }
-        const subMenuList = item.subMenuList
-        if (subMenuList) {
-          subMenuList.forEach((subMenu: any) => {
-            if (subMenu.selected) {
-              this.defaultCheckedKeys.push(subMenu.id)
-            }
-          })
-        }
-      })
-      this.$nextTick(() => {
-        (this.$refs.menuNodes as any).setCheckedKeys(this.defaultCheckedKeys)
-      })
-    }
   }
+  // created () {
+  //   this.loadmMenuNodeList()
+  //   this.loadCheckedMenu()
+  // },
+  // methods: {
+  //   async handleUpdate () {
+  //     const checkedKeys = (this.$refs.menuNodes as any).getCheckedKeys()
+  //     const { data } = await allocateMenus({
+  //       menuIdList: checkedKeys,
+  //       roleId: (this.$route as any).params.id
+  //     })
+  //     console.log(data)
+  //   },
+  //   handleClear () {
+  //     this.$nextTick(() => {
+  //       (this.$refs.menuNodes as any).setCheckedKeys([])
+  //     })
+  //   },
+  //   async loadmResourceNodeList () {
+  //     const { data } = await getMenuNodeList()
+  //     if (data.code === '000000') {
+  //       this.menuNodes = data.data
+  //       // this.loadCheckedMenu()
+  //     }
+  //   },
+  //   async loadCheckedMenu () {
+  //     const roleId = (this.$route as any).params.id
+  //     const { data } = await getRoleMenu(roleId)
+  //     const roleMenuList = data.data
+  //     roleMenuList.forEach((item: any) => {
+  //       if (item.selected) {
+  //         this.defaultCheckedKeys.push(item.id)
+  //       }
+  //       const subMenuList = item.subMenuList
+  //       if (subMenuList) {
+  //         subMenuList.forEach((subMenu: any) => {
+  //           if (subMenu.selected) {
+  //             this.defaultCheckedKeys.push(subMenu.id)
+  //           }
+  //         })
+  //       }
+  //     })
+  //     this.$nextTick(() => {
+  //       (this.$refs.menuNodes as any).setCheckedKeys(this.defaultCheckedKeys)
+  //     })
+  // }
+  // }
 })
 </script>
 <style scoped lang = 'sass'>
